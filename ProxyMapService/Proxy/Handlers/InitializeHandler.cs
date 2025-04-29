90f7d727f45d85c7d0ba702f6669ba46394920f8
vitaly <optinsoft@gmail.com>
2025-04-29 18:28:15 +0500

    InitializeHandler: catch Header exception

--- ProxyMapService/Proxy/Handlers/InitializeHandler.cs
+++ ProxyMapService/Proxy/Handlers/InitializeHandler.cs
@@ -13,32 +13,30 @@ namespace ProxyMapService.Proxy.Handlers
             {
                 context.ClientStream = context.Client.GetStream();
                 context.HeaderBytes = await context.ClientHeaderStream.ReadHeaderBytes(context.ClientStream, context.Token);
+                if (context.HeaderBytes != null)
+                {
+                    switch (context.ClientHeaderStream.SocksVersion)
+                    {
+                        case 0x00:
+                            context.Http = new HttpRequestHeader(context.HeaderBytes);
+                            return HandleStep.HttpInitialized;
+                        case 0x04:
+                            context.Socks4 = new Socks4Header(context.HeaderBytes);
+                            if (context.Socks4.IsConnectRequest(context.HeaderBytes))
+                            {
+                                return HandleStep.Socks4Initialized;
+                            }
+                            break;
+                        case 0x05:
+                            context.Socks5 = new Socks5Header(context.HeaderBytes);
+                            return HandleStep.Socks5Initialized;
+                    }
+                }
             }
             catch (Exception)
             {
                 context.SessionsCounter?.OnHeaderFailed(context);
                 throw;
-            }
-            if (context.HeaderBytes == null)
-            {
-                context.SessionsCounter?.OnHeaderFailed(context);
-                return HandleStep.Terminate;
-            }
-            switch (context.ClientHeaderStream.SocksVersion)
-            {
-                case 0x00:
-                    context.Http = new HttpRequestHeader(context.HeaderBytes);
-                    return HandleStep.HttpInitialized;
-                case 0x04:
-                    context.Socks4 = new Socks4Header(context.HeaderBytes);
-                    if (context.Socks4.IsConnectRequest(context.HeaderBytes))
-                    {
-                        return HandleStep.Socks4Initialized;
-                    }
-                    break;
-                case 0x05:
-                    context.Socks5 = new Socks5Header(context.HeaderBytes);
-                    return HandleStep.Socks5Initialized;
             }
             context.SessionsCounter?.OnHeaderFailed(context);
             return HandleStep.Terminate;
