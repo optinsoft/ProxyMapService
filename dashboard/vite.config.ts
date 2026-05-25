8a072f226cdb80635dda52c942e581b03c33bfc1
vitaly <optinsoft@gmail.com>
2026-05-25 19:12:13 +0500

    base: /ProxyMapDashboard/

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => {
   const login_password: string = env.LOGIN_PASSWORD || 'test';
 
   return {
+    base: '/ProxyMapDashboard/', 
     plugins: [
       vue(),
       vueDevTools(),
